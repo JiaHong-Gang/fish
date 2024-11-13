7ae55738d6f52aa2c4f9b218876bb5232a82f3ca
gangjiahong <1491945882@qq.com>
2024-11-13 15:47:48 +0900

    saturation has been changed

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import pandas as pd
-from resnet_model import ResNet_18
+#import pandas as pd
+#from resnet_model import ResNet_18
 
 from tensorflow.python.keras.combinations import generate
 """"
@@ -35,8 +35,8 @@ plt.xlabel("value")
 plt.tight_layout()
 plt.show()
 """
-hue = (15, 30)
-saturation = (240, 253)
+hue = (97, 110)
+saturation = (110, 150)
 value = (220, 255)
 num_images = 10
 generate_images = []
@@ -62,7 +62,7 @@ for i, ax in enumerate(axes.flat):
     ax.axis("off")
 plt.tight_layout()
 plt.show()
-
+""""
 model = ResNet_18()
 model.load_weights("/home/gou/Programs/fish/result/model_weight.h5")
 csv_path = "/home/gou/Programs/fish/result/predict_blue.csv"
@@ -84,6 +84,6 @@ for i, img in enumerate(generate_images):
 df = pd.DataFrame(result)
 df.to_csv(csv_path, index = False)
 print(f"data saved in {csv_path}")
-
+"""
 
 
