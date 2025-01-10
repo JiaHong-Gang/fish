7198eb18077bccde9337834228fde921ff33b2fa
gangjiahong <1491945882@qq.com>
2025-01-10 16:58:34 +0900

    fixed bug

--- train.py
+++ train.py
@@ -17,37 +17,38 @@ def train_step(model, x_batch, optimizer):
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     return loss
 
-def train_model(x_train,x_val, model):
+def train_model(x_train,x_val, model, strategy):
     #train dataset
-    train_dataset = tf.data.Dataset.from_tensor_slices(x_train)
-    train_dataset = train_dataset.cache()
-    train_dataset = train_dataset.shuffle(buffer_size = 1000)
-    train_dataset = train_dataset.batch(batch_size)
-    train_dataset = train_dataset.prefetch(buffer_size = tf.data.AUTOTUNE)
-    # validation dataset
-    val_dataset = tf.data.Dataset.from_tensor_slices(x_val)
-    val_dataset = val_dataset.cache()
-    val_dataset = val_dataset.batch(batch_size)
-    val_dataset = val_dataset.prefetch(buffer_size = tf.data.AUTOTUNE)
-    optimizer = Adam(learning_rate = 0.0001)
-    #train history
-    train_losses = []
-    val_losses = []
-    for epoch in range(epochs):
-        print(f"\nEpoch {epoch + 1} / epochs")
-        train_loss = 0.0
-        val_loss = 0.0
-        for step, x_batch_train in enumerate(train_dataset):
-            loss = train_step(model, x_batch_train, optimizer)
-            train_loss += loss.numpy()
-        for x_batch_val in val_dataset:
-            y_pred, z_mean, z_log_var = model(x_batch_val, training = False)
-            val_loss = vae_loss(x_batch_val, y_pred, z_mean, z_log_var)
-            val_loss += val_loss.numpy()
-        epoch_train_loss = train_loss / len(train_dataset)
-        epoch_val_loss = val_loss / len(val_dataset)
-        print(f"Training loss : {epoch_train_loss:.4f}")
-        print(f"Validation loss : {epoch_val_loss:.4f}")
+    with strategy.scope():
+        train_dataset = tf.data.Dataset.from_tensor_slices(x_train)
+        train_dataset = train_dataset.cache()
+        train_dataset = train_dataset.shuffle(buffer_size=1000)
+        train_dataset = train_dataset.batch(batch_size)
+        train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+        # validation dataset
+        val_dataset = tf.data.Dataset.from_tensor_slices(x_val)
+        val_dataset = val_dataset.cache()
+        val_dataset = val_dataset.batch(batch_size)
+        val_dataset = val_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+        optimizer = Adam(learning_rate=0.0001)
+        # train history
+        train_losses = []
+        val_losses = []
+        for epoch in range(epochs):
+            print(f"\nEpoch {epoch + 1} / epochs")
+            train_loss = 0.0
+            val_loss = 0.0
+            for step, x_batch_train in enumerate(train_dataset):
+                loss = train_step(model, x_batch_train, optimizer)
+                train_loss += loss.numpy()
+            for x_batch_val in val_dataset:
+                y_pred, z_mean, z_log_var = model(x_batch_val, training=False)
+                val_loss = vae_loss(x_batch_val, y_pred, z_mean, z_log_var)
+                val_loss += val_loss.numpy()
+            epoch_train_loss = train_loss / len(train_dataset)
+            epoch_val_loss = val_loss / len(val_dataset)
+            print(f"Training loss : {epoch_train_loss:.4f}")
+            print(f"Validation loss : {epoch_val_loss:.4f}")
 
-    model.save("/home/gou/Programs/fish/result/model.h5")
+        model.save("/home/gou/Programs/fish/result/model.h5")
     return  model, train_losses, val_losses
