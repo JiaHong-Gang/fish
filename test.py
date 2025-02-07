7b1ff75a6f2bde0fd507e4547ac5737f01d129c1
JiaHong-Gang <gjh985330345@gmail.com>
2025-02-07 16:13:46 +0900

    image test experiment is over

--- test.py
+++ test.py
@@ -51,8 +51,8 @@ def test_model_and_calculate_mse_difference(model, folder_original, folder_proce
     mse_original = calculate_mse(original_images[:num_samples], reconstructed_original)
     mse_processed = calculate_mse(processed_images[:num_samples], reconstructed_processed)
 
-    print(f"✅ 原始图像 MSE: {mse_original:.4f}")
-    print(f"✅ 处理后图像 MSE: {mse_processed:.4f}")
+    print(f"✅ 原始图像 MSE: {mse_original:.8f}")
+    print(f"✅ 处理后图像 MSE: {mse_processed:.8f}")
 
     # 保存图像
     plt.figure(figsize=(15, 10))
@@ -82,8 +82,8 @@ def test_model_and_calculate_mse_difference(model, folder_original, folder_proce
     plt.show()
 
 # 路径配置
-folder_original = "/home/gou/Programs/fish/test/OriginalImages"  # 替换为原始图像文件夹路径
-folder_processed = "/home/gou/Programs/fish/test/ProcessedImages"  # 替换为处理后图像文件夹路径
+folder_original = "/home/gou/Programs/fish/test_image/rotation_image_test/original_image"  # 替换为原始图像文件夹路径
+folder_processed = "/home/gou/Programs/fish/test_image/rotation_image_test/rotation_image"  # 替换为处理后图像文件夹路径
 save_path = "/home/gou/Programs/fish/test"  # 替换为结果保存路径
 
 # 加载模型
