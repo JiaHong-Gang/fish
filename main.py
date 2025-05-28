2889daa15bc627934d6647fbddac9a190b81a51c
gangjiahong <1491945882@qq.com>
2025-05-28 17:47:04 +0900

    fixed some bug

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ import tensorflow as tf
 import os
 import numpy as np
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.optimizers import Adam
+os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
+
+gpus = tf.config.list_physical_devices("GPU")
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print("Memory growth setting failed:", e)
 from config import ht_img, wd_img, epochs, batch_size
 from load_image import load_images
 from process_image import process_image
-
 from vae_model import vae
 from train_step import Training
-from tensorflow.keras.optimizers import Adam
 from train_log import learning_curve, training_log
 
-os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
-
-os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
 def main():
 #-----------------------check GPU---------------------------
-    gpus = tf.config.list_physical_devices("GPU")
     print(f"available GPUs {len(gpus)}")
     for gpu in gpus:
         print(gpu)
-        tf.config.experimental.set_memory_growth(gpu, True)
-#----------------------use all GPU-------------------------
     print("Is GPU available:", tf.config.list_logical_devices('GPU'))
+#----------------------programs-------------------------
     images= load_images()  # load images
     images = process_image(images)  # process images
     x_train, x_val = train_test_split(images, test_size=0.2, random_state=42)  # split dataset 80% for training 20% for validation
